1d0cfd7fcc4c6399646bdb77b5f56a2f9b52bb92
ranje <ranje@Ranjeet>
2023-09-01 22:30:06 +0530

    intial commit

---
+++ IoPackageEx1/src/com/ranjeet/MainApp.java
@@ -0,0 +1,30 @@
+package com.ranjeet;
+
+
+
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class MainApp 
+
+{
+	public static void main(String[] args)throws IOException
+	{
+	File f =new File("C:/demo/sample.txt");
+	if(f.exists())
+	{
+		System.out.println("Already Exist");
+		
+	}
+	else
+	{
+		f.createNewFile();
+		System.out.println("New File Created");
+		
+		
+	}
+
+}
+}
---
+++ IoPackageEx1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module IoPackageEx1 {
+}
